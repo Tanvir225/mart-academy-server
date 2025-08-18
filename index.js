eec21f73a5c42156f2250e8655fc50bbe973c187
Tanvir225 <tanvirrehman200@gmail.com>
2025-08-18 16:30:28 +0600

    cors issue has been added

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ require('dotenv').config();
 // Middleware
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: ['http://localhost:5173','http://hideous-ray.surge.sh'],
         credentials: true,
     }
 ));
